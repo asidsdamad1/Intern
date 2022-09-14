1c97d751cce771deaf2ba0e8b52ab67958aead8e
hieu <trunghieu.ngo612@gmail.com>
2022-09-14 11:02:23 +0700

    add hibernate demo

--- mapping/src/main/java/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.Account;
-
-public interface AccountService {
-    Account addAcount(String name, double amount);
-    void listAccounts();
-    Account updateAccount(Long id, double amount);
-}
