ea8b3dff7cc54a9fc6a6550873e40cf0ecdb3eb5
hieu <trunghieu.ngo612@gmail.com>
2022-10-11 15:47:06 +0700

    delete vehicle

--- spring/vehicle/src/main/java/com/example/vehicle/service/VehicleInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.vehicle.service;
-
-import com.example.vehicle.dto.VehicleInfoDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface VehicleInfoService {
-    VehicleInfoDto getByPlate(String plate);
-}
