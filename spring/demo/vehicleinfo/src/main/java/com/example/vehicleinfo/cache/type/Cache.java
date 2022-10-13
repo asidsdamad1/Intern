9aa8a114000f314559b195d3b77c02b8fb6a57f7
hieu <trunghieu.ngo612@gmail.com>
2022-10-13 11:06:28 +0700

    update

--- spring/demo/vehicleinfo/src/main/java/com/example/vehicleinfo/cache/type/Cache.java
+++ spring/demo/vehicleinfo/src/main/java/com/example/vehicleinfo/cache/type/Cache.java
@@ -1,7 +1,9 @@
 package com.example.vehicleinfo.cache.type;
 
 public interface Cache {
-    public void setValue(final String key, Object object);
-    public String getValue(final String key);
-    public void deleteKey(String key);
+    void setValue(final String key, Object object);
+
+    String getValue(final String key);
+
+    void deleteKey(String key);
 }
